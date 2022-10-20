4874ca50661d0de7b1b4ca88af366c1ba9f1880f
Anton Holst <holst9559@hotmail.com>
2022-10-20 12:26:22 +0200

    Sista ändringar, projektet är klart

--- script.js
+++ script.js
@@ -2,14 +2,10 @@ console.log("JS laddat");
 
 const ulList = document.getElementById("ulList");
 const input = document.getElementById("input");
-const msg = "Input must not be empty";
 const inputButton = document.getElementById("inputButton");
 const completed = document.getElementById("completed");
-
+const msg = "Input must not be empty";
 let completedCount = 0;
-
-
-
 
 inputButton.addEventListener("click", function (e) {
     e.preventDefault();
@@ -20,34 +16,40 @@ inputButton.addEventListener("click", function (e) {
         warning.innerHTML = "";
         input.value = "";
 
+        //Skapar li, span och button element
         const inputLi = document.createElement("li");
         const liContent = document.createElement("span");
         const tcButton = document.createElement("button");
         
-        inputLi.setAttribute("class", "inputLi");
+        //Ger de nya elementen en class
         liContent.setAttribute("class", "liContent");
         tcButton.setAttribute("class", "tcButton");
-
         liContent.innerText = textInput;
 
+        //Lägger in alla element i ul li
         ulList.appendChild(inputLi);
         inputLi.appendChild(liContent);
         inputLi.appendChild(tcButton);
 
+        //En liten delay när ett nytt listelement skapas, kändes mer smooth
+        setTimeout(function() {
+            inputLi.setAttribute("class", "fade");
+        }, 20)
 
+        //Funktion för att ta bort en todo
         tcButton.addEventListener("click", function () {
             if (liContent.getAttribute("class") == ("completed")){
-                //Tar bort borttagna ToDos från completed 
+                //Tar bort borttagna ToDos från completed
+                //(tog bort koden eftersom det var inte så det var kodat på videon)
                 //completedCount--;
                 //completed.innerText = completedCount + " completed";
                 inputLi.remove();
             }
             else {
                 inputLi.remove();
-
             }});
 
-
+        //Funktion för att markera en todo som completed eller inte completed samt räknare
         liContent.addEventListener("click", function () {
             if (liContent.getAttribute("class") == ("completed")){
                 liContent.setAttribute("class", "");
@@ -59,18 +61,17 @@ inputButton.addEventListener("click", function (e) {
                 completedCount++;
                 completed.innerText = completedCount + " completed";
             }
-
         })
-
-
     }
 
+    //Skickar ut felmeddelande och ger den en class för animation
     else {
+        warning.setAttribute("class", "warningBlink")
         warning.innerHTML = msg;
-        
         return;
     }
-
+    
     textInput.value = ""
-
+    //Återställer animationen
+    warning.setAttribute("class", "")
 } );
